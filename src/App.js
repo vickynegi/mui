5b275440629ad42566132eed9697cbe9cc3958ff
Vivek Negi <viveknegi@Viveks-MacBook-Pro.local>
2023-12-15 22:54:36 +0530

    feat: removed html

--- src/App.js
+++ src/App.js
@@ -36,26 +36,25 @@ function App() {
         </tr> 
   
         <tr> 
-            {/* <td><input type="button" value="1" onClick={() =>{f4(1)}} /></td>  */}
             <CustomButton value={1} />
-            <td><input type="button" value="2" onClick={() =>{f4(2)}} /></td> 
-            <td><input type="button" value="3" onClick={() =>{f4(3)}} /></td> 
+            <CustomButton value={2} />
+            <CustomButton value={3} />
             <td><input type="button" value="/" onClick={() =>{f4('/')}} /></td> 
         </tr> 
         <tr> 
-            <td><input type="button" value="4" onClick={() =>{f4(4)}} /></td> 
-            <td><input type="button" value="5" onClick={() =>{f4(5)}} /></td> 
-            <td><input type="button" value="6" onClick={() =>{f4(6)}} /></td> 
+            <CustomButton value={4} />
+            <CustomButton value={5} />
+            <CustomButton value={6} />
             <td><input type="button" value="*" onClick={() =>{f4('*')}}/></td> 
         </tr> 
-        <tr> 
-            <td><input type="button" value="7" onClick={() =>{f4(7)}} /></td> 
-            <td><input type="button" value="8" onClick={() =>{f4(8)}} /></td> 
-            <td><input type="button" value="9" onClick={() =>{f4(9)}} /></td> 
+        <tr>
+            <CustomButton value={7} />
+            <CustomButton value={8} />
+            <CustomButton value={9} />
             <td><input type="button" value="-" onClick={() =>{f4('-')}}/></td> 
         </tr> 
-        <tr> 
-            <td><input type="button" value="0" onClick={() =>{f4(0)}} /></td> 
+        <tr>
+            <CustomButton value={0} />
             <td><input type="button" value="." onClick={() =>{f4('.')}} /></td> 
             <td><input type="button" value="=" onClick={() =>{result()}}/></td> 
             <td><input type="button" value="+" onClick={() =>{f4('+')}}/></td> 
